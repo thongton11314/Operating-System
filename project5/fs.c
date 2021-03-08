e748242fb9e468a5dc40ba9c40d6077947336c7b
bb10789 <chakhim.kam@gmail.com>
2021-03-08 15:55:52 -0800

    Read Passed

--- project5/fs.c
+++ project5/fs.c
@@ -8,81 +8,98 @@
 // ============================================================================
 // Close the file currently open on file descriptor 'fd'.
 // ============================================================================
-i32 fsClose(i32 fd) { 
+i32 fsClose(i32 fd)
+{
   i32 inum = bfsFdToInum(fd);
   bfsDerefOFT(inum);
-  return 0; 
+  return 0;
 }
-
-
 
 // ============================================================================
 // Create the file called 'fname'.  Overwrite, if it already exsists.
 // On success, return its file descriptor.  On failure, EFNF
 // ============================================================================
-i32 fsCreate(str fname) {
+i32 fsCreate(str fname)
+{
   i32 inum = bfsCreateFile(fname);
-  if (inum == EFNF) return EFNF;
+  if (inum == EFNF)
+    return EFNF;
   return bfsInumToFd(inum);
 }
 
-
-
 // ============================================================================
-// Format the BFS disk by initializing the SuperBlock, Inodes, Directory and 
+// Format the BFS disk by initializing the SuperBlock, Inodes, Directory and
 // Freelist.  On succes, return 0.  On failure, abort
 // ============================================================================
-i32 fsFormat() {
-  FILE* fp = fopen(BFSDISK, "w+b");
-  if (fp == NULL) FATAL(EDISKCREATE);
+i32 fsFormat()
+{
+  FILE *fp = fopen(BFSDISK, "w+b");
+  if (fp == NULL)
+    FATAL(EDISKCREATE);
 
-  i32 ret = bfsInitSuper(fp);               // initialize Super block
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  i32 ret = bfsInitSuper(fp); // initialize Super block
+  if (ret != 0)
+  {
+    fclose(fp);
+    FATAL(ret);
+  }
 
-  ret = bfsInitInodes(fp);                  // initialize Inodes block
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  ret = bfsInitInodes(fp); // initialize Inodes block
+  if (ret != 0)
+  {
+    fclose(fp);
+    FATAL(ret);
+  }
 
-  ret = bfsInitDir(fp);                     // initialize Dir block
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  ret = bfsInitDir(fp); // initialize Dir block
+  if (ret != 0)
+  {
+    fclose(fp);
+    FATAL(ret);
+  }
 
-  ret = bfsInitFreeList();                  // initialize Freelist
-  if (ret != 0) { fclose(fp); FATAL(ret); }
+  ret = bfsInitFreeList(); // initialize Freelist
+  if (ret != 0)
+  {
+    fclose(fp);
+    FATAL(ret);
+  }
 
   fclose(fp);
   return 0;
 }
-
 
 // ============================================================================
 // Mount the BFS disk.  It must already exist
 // ============================================================================
-i32 fsMount() {
-  FILE* fp = fopen(BFSDISK, "rb");
-  if (fp == NULL) FATAL(ENODISK);           // BFSDISK not found
+i32 fsMount()
+{
+  FILE *fp = fopen(BFSDISK, "rb");
+  if (fp == NULL)
+    FATAL(ENODISK); // BFSDISK not found
   fclose(fp);
   return 0;
 }
 
-
-
 // ============================================================================
-// Open the existing file called 'fname'.  On success, return its file 
+// Open the existing file called 'fname'.  On success, return its file
 // descriptor.  On failure, return EFNF
 // ============================================================================
-i32 fsOpen(str fname) {
-  i32 inum = bfsLookupFile(fname);        // lookup 'fname' in Directory
-  if (inum == EFNF) return EFNF;
+i32 fsOpen(str fname)
+{
+  i32 inum = bfsLookupFile(fname); // lookup 'fname' in Directory
+  if (inum == EFNF)
+    return EFNF;
   return bfsInumToFd(inum);
 }
-
-
 
 // ============================================================================
 // Read 'numb' bytes of data from the cursor in the file currently fsOpen'd on
 // File Descriptor 'fd' into 'buf'.  On success, return actual number of bytes
 // read (may be less than 'numb' if we hit EOF).  On failure, abort
 // ============================================================================
-i32 fsRead(i32 fd, i32 numb, void* buf) {
+i32 fsRead(i32 fd, i32 numb, void *buf)
+{
 
   // ++++++++++++++++++++++++
   // Insert your code here
@@ -98,28 +115,45 @@ i32 fsRead(i32 fd, i32 numb, void* buf) {
 
   // Find the inum
   i32 inum = bfsFdToInum(fd);
-
+  i32 count = 0;
   // Check size of Inode
-  if (bfsGetSize(inum) >= numb) {
-
+  if (bfsGetSize(inum) >= numb)
+  {
+    i32 totalSize = numb;
     // find the current cursor
     i32 cursor = bfsTell(fd);
+    i32 currFbn = cursor / BYTESPERBLOCK; //get fileblock number, floored
+    do
+    {
+      if (totalSize > BYTESPERBLOCK) // 1 block = max of 512 bytes, get a max of 512 or the remainder of total bytes
+      {
+        numb = BYTESPERBLOCK;
+        totalSize -= BYTESPERBLOCK;
+      }
+      else
+      {
+        numb = totalSize;
+        totalSize = 0;
+      }
 
-    // Read
-    bfsRead(inum, cursor, buf);
+      i8 * tempBuf[BUFSIZ]; //used to temporary store buffer and put it back in later
+      // Read 
+      bfsRead(inum, currFbn, tempBuf);
+      memmove(buf + count, tempBuf, numb);
 
-    // Set new cursor
-    bfsSetCursor(inum, numb);
+      // Set new cursor
+      bfsSetCursor(inum, cursor += numb);
+      count += numb; // increment total size read
+      currFbn++; // increment File Block number
+    } while (totalSize > 0);
 
-    // return the amount of read
-    return numb;
+  return count;
   }
 
   // Look of the file
-  FATAL(ENYI);                                  // Not Yet Implemented!
+  FATAL(ENYI); // Not Yet Implemented!
   return 0;
 }
-
 
 // ============================================================================
 // Move the cursor for the file currently open on File Descriptor 'fd' to the
@@ -131,65 +165,66 @@ i32 fsRead(i32 fd, i32 numb, void* buf) {
 //
 // On success, return 0.  On failure, abort
 // ============================================================================
-i32 fsSeek(i32 fd, i32 offset, i32 whence) {
+i32 fsSeek(i32 fd, i32 offset, i32 whence)
+{
 
-  if (offset < 0) FATAL(EBADCURS);
- 
+  if (offset < 0)
+    FATAL(EBADCURS);
+
   i32 inum = bfsFdToInum(fd);
   i32 ofte = bfsFindOFTE(inum);
-  
-  switch(whence) {
-    case SEEK_SET:
-      g_oft[ofte].curs = offset;
-      break;
-    case SEEK_CUR:
-      g_oft[ofte].curs += offset;
-      break;
-    case SEEK_END: {
-        i32 end = fsSize(fd);
-        g_oft[ofte].curs = end + offset;
-        break;
-      }
-    default:
-        FATAL(EBADWHENCE);
+
+  switch (whence)
+  {
+  case SEEK_SET:
+    g_oft[ofte].curs = offset;
+    break;
+  case SEEK_CUR:
+    g_oft[ofte].curs += offset;
+    break;
+  case SEEK_END:
+  {
+    i32 end = fsSize(fd);
+    g_oft[ofte].curs = end + offset;
+    break;
+  }
+  default:
+    FATAL(EBADWHENCE);
   }
   return 0;
 }
 
-
-
 // ============================================================================
 // Return the cursor position for the file open on File Descriptor 'fd'
 // ============================================================================
-i32 fsTell(i32 fd) {
+i32 fsTell(i32 fd)
+{
   return bfsTell(fd);
 }
-
-
 
 // ============================================================================
 // Retrieve the current file size in bytes.  This depends on the highest offset
 // written to the file, or the highest offset set with the fsSeek function.  On
 // success, return the file size.  On failure, abort
 // ============================================================================
-i32 fsSize(i32 fd) {
+i32 fsSize(i32 fd)
+{
   i32 inum = bfsFdToInum(fd);
   return bfsGetSize(inum);
 }
-
-
 
 // ============================================================================
 // Write 'numb' bytes of data from 'buf' into the file currently fsOpen'd on
 // filedescriptor 'fd'.  The write starts at the current file offset for the
 // destination file.  On success, return 0.  On failure, abort
 // ============================================================================
-i32 fsWrite(i32 fd, i32 numb, void* buf) {
+i32 fsWrite(i32 fd, i32 numb, void *buf)
+{
 
   // ++++++++++++++++++++++++
   // Insert your code here
   // ++++++++++++++++++++++++
 
-  FATAL(ENYI);                                  // Not Yet Implemented!
+  FATAL(ENYI); // Not Yet Implemented!
   return 0;
 }
